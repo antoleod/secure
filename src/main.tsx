120716199e5cfbe971bdf45fe71518d01207cd70
Gean Dioses <40503619+antoleod@users.noreply.github.com>
2026-01-23 15:14:00 +0100

    Funcioanndo antes de anadir boton

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,24 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.tsx';
 import { I18nProvider } from './contexts/I18nContext';
 import { ErrorBoundary } from './components/ErrorBoundary';
 import './index.css';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
-      <ErrorBoundary>
-        <I18nProvider>
-          <App />
-        </I18nProvider>
-      </ErrorBoundary>
-    </HashRouter>
+    <QueryClientProvider client={queryClient}>
+      <HashRouter>
+        <ErrorBoundary>
+          <I18nProvider>
+            <App />
+          </I18nProvider>
+        </ErrorBoundary>
+      </HashRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
